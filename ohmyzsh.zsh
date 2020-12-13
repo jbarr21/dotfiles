dbd7ee691793d51d24effc239f60eaedcf0925b4
James Barr <barr.james.r@gmail.com>
2020-12-12 18:06:52 -0800

    Add bootstrap & resources

--- ohmyzsh.zsh
+++ ohmyzsh.zsh
@@ -117,5 +117,3 @@ source $ZSH/oh-my-zsh.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-[ -r /Users/jbarr/.profile_lda ] && . /Users/jbarr/.profile_lda
