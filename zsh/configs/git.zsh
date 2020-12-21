f50663f7c247c73e58a26d3535d776b671748e70
James Barr <barr.james.r@gmail.com>
2020-12-20 17:18:06 -0800

    Fix install script paths & add more packages

--- zsh/configs/git.zsh
+++ zsh/configs/git.zsh
@@ -1,2 +1,2 @@
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global core.pager "diff-so-fancy | less --tabs=1,5 -RFX"
 git config --global pager.show "diff-so-fancy | less --tabs=1,5 -RFX"
