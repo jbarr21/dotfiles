79708c0c66728def174e2e3e39fe533fee29b411
James Barr <barr.james.r@gmail.com>
2020-12-27 14:42:53 -0800

    Fix zsh path & increase history size

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -1,6 +1,7 @@
 setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zsh_history
-HISTSIZE=20000
-SAVEHIST=20000
+HISTORY_IGNORE='(l|ls|ll|cd|cd ..|pwd|exit|date|history)'
+HISTSIZE=290000
+SAVEHIST=$HISTSIZE
 
 export ERL_AFLAGS="-kernel shell_history enabled"
