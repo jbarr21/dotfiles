4a599d53b53dfa197366e8068b4c5c1dcdfef938
James Barr <barr.james.r@gmail.com>
2021-10-11 16:18:47 -0700

    Cache vim plug update

--- zsh/configs/vim.zsh
+++ zsh/configs/vim.zsh
@@ -1,10 +1,15 @@
-if [ -e "$HOME"/.vim/autoload/plug.vim ]; then
-  vim -E -s +PlugUpgrade +qa
-else
-  curl -fLo "$HOME"/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+PREV_MD5="$(test -e "$HOME/.vimrc.bundles.md5" && \cat $HOME/.vimrc.bundles.md5 || echo '0')"
+CURR_MD5="$(md5sum "$HOME/.vimrc.bundles" | cut -d ' ' -f 1)"
+
+if [ $PREV_MD5 != $CURR_MD5 ]; then
+  if [ -e "$HOME"/.vim/autoload/plug.vim ]; then
+    vim -E -s +PlugUpgrade +qa
+  else
+    curl -fLo "$HOME"/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  fi
+
+  vim -u "$HOME"/.vimrc.bundles +PlugUpdate +PlugClean! +qa
+
+  echo "$CURR_MD5" > "$HOME/.vimrc.bundles.md5"
 fi
-
-vim -u "$HOME"/.vimrc.bundles +PlugUpdate +PlugClean! +qa
-
-reset -Q
