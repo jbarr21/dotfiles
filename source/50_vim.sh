fb9dd586a94fd5a7421e5151fdb770fa141af5d9
James Barr <barr.james.r@gmail.com>
2023-01-11 00:25:55 -0800

    Don't source vimplug when vim is nvim

--- source/50_vim.sh
+++ source/50_vim.sh
@@ -1,15 +1,17 @@
-PREV_MD5="$(test -e "$HOME/.vimrc.bundles.md5" && \cat $HOME/.vimrc.bundles.md5 || echo '0')"
-CURR_MD5="$(md5sum "$HOME/.vimrc.bundles" | cut -d ' ' -f 1)"
+if [[ $(vim --version | head -n 1 | grep -i nvim | wc -l) -eq 0 ]]; then
+  PREV_MD5="$(test -e "$HOME/.vimrc.bundles.md5" && \cat $HOME/.vimrc.bundles.md5 || echo '0')"
+  CURR_MD5="$(md5sum "$HOME/.vimrc.bundles" | cut -d ' ' -f 1)"
 
-if [ $PREV_MD5 != $CURR_MD5 ]; then
+  if [ $PREV_MD5 != $CURR_MD5 ]; then
   if [ -e "$HOME"/.vim/autoload/plug.vim ]; then
-    vim -E -s +PlugUpgrade +qa
+      vim -E -s +PlugUpgrade +qa
   else
-    curl -fLo "$HOME"/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+      curl -fLo "$HOME"/.vim/autoload/plug.vim --create-dirs \
+          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
 
   vim -u "$HOME"/.vimrc.bundles +PlugUpdate +PlugClean! +qa
 
   echo "$CURR_MD5" > "$HOME/.vimrc.bundles.md5"
+  fi
 fi
