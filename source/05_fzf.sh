93209d8e644cfec850f326c806345780b3e09866
James Barr <barr.james.r@gmail.com>
2023-07-17 22:39:26 -0700

    Limit FZF to non-ignored files

--- source/05_fzf.sh
+++ source/05_fzf.sh
@@ -1,6 +1,6 @@
 # add support for ctrl+o to open selected file in VS Code
 export FZF_DEFAULT_OPTS=""
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS='--preview="$HOME/dotfiles/bin/preview {}" --preview-window=right:60%:wrap'
 export FZF_ALT_C_OPTS='--preview="HOME/dotfiles/bin/preview {}" --preview-window=right:60%:wrap'
