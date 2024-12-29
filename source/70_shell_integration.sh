150005aec40cef138d21f3bad7f424bc00fdb176
James Barr <barr.james.r@gmail.com>
2024-12-28 19:07:12 -0800

    Add ghostty shell integration

---
+++ source/70_shell_integration.sh
@@ -0,0 +1,2 @@
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+test -n "$GHOSTTY_RESOURCES_DIR" && source $GHOSTTY_RESOURCES_DIR/shell-integration/zsh/ghostty-integration
