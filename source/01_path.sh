ef4db4076b49889a5a1ad9d5c9b048bf6b0b8c78
James Barr <barr.james.r@gmail.com>
2024-12-28 19:07:26 -0800

    Add homebrew paths to path

--- source/01_path.sh
+++ source/01_path.sh
@@ -1,3 +1,5 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$DOTFILES/bin:$HOME/bin:/usr/local/sbin:$PATH"
+PATH="/opt/homebrew/bin:$PATH"
+PATH="/opt/homebrew/sbin:$PATH"
 export -U PATH
