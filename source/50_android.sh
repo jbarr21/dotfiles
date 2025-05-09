1ec9f8529621fb1a9c726c608f0d828504f1b686
James Barr <jbarr@uber.com>
2025-05-09 14:21:55 -0700

    Update JAVA_HOME to 17

--- source/50_android.sh
+++ source/50_android.sh
@@ -2,11 +2,12 @@ if [ -z "$DEVPOD_NAME" ]; then
   if [ $(uname -s) = "Darwin" ]; then
     unset JAVA_HOME
     export JDK_16="$(/usr/libexec/java_home -v1.6)"
-    export JDK_17="$(/usr/libexec/java_home -v1.7)"
     export JDK_18="$(/usr/libexec/java_home -v1.8)"
     export JDK_9="$(/usr/libexec/java_home -v9)"
     export JDK_11="$(/usr/libexec/java_home -v11)"
-    export JAVA_HOME=$JDK_11
+    export JDK_17="$(/usr/libexec/java_home -v17)"
+    export JDK_21="$(/usr/libexec/java_home -v21)"
+    export JAVA_HOME=$JDK_17
   else
     export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
   fi
@@ -15,6 +16,8 @@ if [ -z "$DEVPOD_NAME" ]; then
   export ANDROID_NDK_HOME=~/android-ndk
 
   export PATH=$JAVA_HOME/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:~/.local/bin:$ANDROID_HOME/platform-tools:$PATH
+else
+  export JAVA_HOME="/opt/jvm/jdk-17"
 fi
 
 function bw() {
