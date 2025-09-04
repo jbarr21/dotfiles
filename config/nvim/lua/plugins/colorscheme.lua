6de9d1963aabf6f20870ea23e6a3800a94b390f2
James Barr <jbarr@uber.com>
2025-09-04 00:21:55 -0700

    Catppuccin bufferline fix

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,17 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+		"catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    -- Until LazyVim/LazyVim/issues/6355 resolved:
+		opts = function(_, opts)
+			local module = require("catppuccin.groups.integrations.bufferline")
+			if module then
+				module.get = module.get_theme
+			end
+			return opts
+		end,
+	},
 
   -- Configure LazyVim to load theme
   {
