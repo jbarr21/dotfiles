3512e464091ccb3a08f38f4d16830bcd4cc08827
James Barr <jbarr@uber.com>
2026-06-11 17:46:28 -0700

    Remove all AI plugins except Avante

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -1,43 +1,79 @@
 return {
   {
-      "olimorris/codecompanion.nvim",
-      dependencies = {
-          "nvim-lua/plenary.nvim",
-          "nvim-treesitter/nvim-treesitter",
-      },
-      opts = {
-  --     system_prompt = {
-  --       enabled = true, -- Enable the tools system prompt?
-  --       replace_main_system_prompt = false, -- Replace the main system prompt with the tools system prompt?
-  --     },
-          strategies = {
-              chat = {
-                  adapter = "claude_code",
-              },
-  --       chat = {
-  --         adapter = "copilot",
-  --       },
-  --       inline = {
-  --         adapter = "copilot",
-  --       },
-  --       agent = {
-  --         adapter = "copilot",
-  --       },
+    "yetone/avante.nvim",
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = vim.fn.has("win32") ~= 0
+        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      provider = "claude-code",
+      acp_providers = {
+        ["claude-code"] = {
+          command = "npx",
+          args = {
+            "--registry",
+            "https://registry.npmjs.org",
+            "acp-claude-code"
           },
+          env = {
+            NODE_NO_WARNINGS = "1",
+            -- ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY"),
+          },
+        },
       },
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    ft = { "markdown", "codecompanion" }
-  },
-  {
-    "ravitemer/mcphub.nvim",
+      -- other configuration options...
+      -- system_prompt = function()
+      --     local hub = require("mcphub").get_hub_instance()
+      --     return hub and hub:get_active_servers_prompt() or ""
+      -- end,
+      -- -- Using function prevents requiring mcphub before it's loaded
+      -- custom_tools = function()
+      --     return {
+      --         require("mcphub.extensions.avante").mcp_tool(),
+      --     }
+      -- end,
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "stevearc/dressing.nvim", -- for input provider dressing
+      "folke/snacks.nvim", -- for input provider snacks
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
     },
-    build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
-    config = function()
-      require("mcphub").setup()
-    end
-  }
+  },
 }
