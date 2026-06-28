c74eaea9776fa1b964c40be215f57f3f627ddc88
James Barr <jbarr@uber.com>
2026-06-28 10:29:56 -0700

    Do not force install of copilot

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,24 @@
 return {
+  {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
+    opts = {
+      automatic_installation = {
+        exclude = { "copilot" },
+      },
     },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      -- LazyVim's ai.sidekick extra adds copilot for NES, but we disable NES.
+      -- Prevent the copilot language server from being configured.
+      if opts.servers and opts.servers.copilot then
+        opts.servers.copilot = nil
+      end
+    end,
+  },
 }
